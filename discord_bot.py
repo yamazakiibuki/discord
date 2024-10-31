9429383f24bd8e7ac244992f646ff028c0abb5bd
やまざき　ふきちゃ <167506737+yamazakiibuki@users.noreply.github.com>
2024-10-31 14:21:05 +0900

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,4 +1,4 @@
-import random
+from utils import random, emphasize
 
 class DiscordBot:
     def __init__(self):
@@ -7,17 +7,17 @@ class DiscordBot:
         self.vc_state_err = '実行できません。ボイスチャンネルに入ってコマンドを実行してください。'
 
     def set_mem(self, ctx):
-        state = ctx.author.voice  # コマンド実行者のVCステータスを取得
+        state = ctx.author.voice
         if state is None:
             return False
-
-        self.channel_mem = [i.name for i in state.channel.members]  # VCメンバリスト取得
-        self.mem_len = len(self.channel_mem)  # 人数取得
+        self.channel_mem = [i.name for i in state.channel.members]
+        self.mem_len = len(self.channel_mem)
         return True
 
     # チーム数を指定した場合のチーム分け
     def make_party_num(self, ctx, party_num):
         team = []
+        remainder = []
 
         if self.set_mem(ctx) is False:
             return self.vc_state_err
@@ -26,9 +26,8 @@ class DiscordBot:
             return '実行できません。チーム分けできる数を指定してください。'
 
         random.shuffle(self.channel_mem)
-
         for i in range(party_num):
-            team.append("=====チーム" + str(i + 1) + "=====")
+            team.append(f"=====チーム {i + 1}=====")
             team.extend(self.channel_mem[i::party_num])
 
         return '\n'.join(team)
