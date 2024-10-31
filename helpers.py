c623acc3541142243ec2378d8177ef5ee0b79cc6
やまざき　ふきちゃ <167506737+yamazakiibuki@users.noreply.github.com>
2024-10-31 14:33:58 +0900

    Update and rename utils.py to helpers.py

---
+++ helpers.py
@@ -0,0 +1,11 @@
+def emphasize(text):
+    return f"**{text}**"
+
+def underline(text):
+    return f"__{text}__"
+
+def isContainedNoInput(command):
+    return any(i == '' for i in command)
+
+list_yesno = ['🙆‍♂️', '🙅‍♂️']
+list_vote = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
