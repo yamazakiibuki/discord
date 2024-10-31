a8839ae94848fbc42545ed7e2230b94912aed9b1
やまざき　ふきちゃ <167506737+yamazakiibuki@users.noreply.github.com>
2024-10-31 14:07:08 +0900

    Create client.py

---
+++ client.py
@@ -0,0 +1,78 @@
+import discord
+from database import save_settings, load_settings
+from utils import list_yesno, list_vote
+from discord_bot import DiscordBot
+
+class MyClient(discord.Client):
+    def __init__(self, intents):
+        super().__init__(intents=intents)
+        self.discord_bot = DiscordBot()
+
+    async def on_ready(self):
+        print('Startup Success!!!')
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        command = message.content.split(".")
+
+        if command[0] == "set_channel":
+            await self.handle_set_channel(message, command)
+
+        elif command[0] == "question":
+            await self.handle_question(message, command)
+
+        elif command[0] == "team":
+            await self.handle_team(message, command)
+
+    async def handle_set_channel(self, message, command):
+        if len(command) < 3:
+            await message.channel.send("チャンネル名を指定してください。")
+            return
+
+        channel_type = command[1]
+        channel_name = command[2]
+        guild_id = message.guild.id
+        channel = discord.utils.get(message.guild.channels, name=channel_name)
+
+        if channel is None:
+            await message.channel.send("指定されたチャンネル名が見つかりません。")
+            return
+
+        if channel_type == "BOT_ROOM":
+            save_settings(guild_id, channel.id, load_settings(guild_id)[1])
+            await message.channel.send(f"ボットルームを `{channel.name}` に設定しました。")
+        elif channel_type == "ANNOUNCE_CHANNEL":
+            _, announce_channel_ids = load_settings(guild_id)
+            announce_channel_ids.append(channel.id)
+            save_settings(guild_id, load_settings(guild_id)[0], announce_channel_ids)
+            await message.channel.send(f"アナウンスチャンネルを `{channel.name}` に設定しました。")
+        else:
+            await message.channel.send("チャンネルタイプが無効です。")
+
+    async def handle_question(self, message, command):
+        if command[1] == "yes-no":
+            embed = discord.Embed(title=command[2], color=discord.Colour.blue())
+            voting_msg = await message.channel.send(embed=embed)
+            for emoji in list_yesno:
+                await voting_msg.add_reaction(emoji)
+        elif command[1] == "vote":
+            embed = discord.Embed(title=command[2], color=discord.Colour.green())
+            vote_candidate = command[3:]
+            for i, candidate in enumerate(vote_candidate):
+                embed.description += f"{list_vote[i]}   {candidate}\n"
+            voting_msg = await message.channel.send(embed=embed)
+            for i in range(len(vote_candidate)):
+                await voting_msg.add_reaction(list_vote[i])
+
+    async def handle_team(self, message, command):
+        team_count = int(command[1]) if len(command) > 1 else 2
+        msg = self.discord_bot.make_party_num(message, team_count)
+        await message.channel.send(msg)
+
+    async def on_member_join(self, member):
+        try:
+            await member.send(f"ようこそ、{member.name}さん！")
+        except Exception as e:
+            print(f"Failed to send welcome message: {e}")
