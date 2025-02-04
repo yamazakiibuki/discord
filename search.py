3f9e5c0234d885be03711d3482ff94167b5f8b6f
やまざき　ふきちゃ <167506737+yamazakiibuki@users.noreply.github.com>
2025-02-04 11:25:54 +0900

    Update search.py

--- search.py
+++ search.py
@@ -1,20 +1,21 @@
-import requests
-from bs4 import BeautifulSoup
-import discord
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 async def search_yahoo_news(query):
     chrome_options = Options()
+    # Chromium の実行ファイルのパスを指定します（環境によっては '/usr/bin/chromium-browser' となる場合もあります）
+    chrome_options.binary_location = "/usr/bin/chromium"
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    service = Service(executable_path='.\\chromedriver-linux64\\chromedriver.exe')  # カレントディレクトリを指定
+
+    # Dockerfile でインストールした chromium-driver が /usr/bin/chromedriver にあるので、そのパスを指定
+    service = Service(executable_path="/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     try:
